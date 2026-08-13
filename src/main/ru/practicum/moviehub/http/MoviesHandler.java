1e09799ba382f98255da3ba2005b9ebeeaa3bc45
egor <egorr12122006@gmail.com>
2026-08-13 17:37:16 +0300

    добавить класс BaseHttpHandler и наследовать от него MoviesHandler

--- src/main/ru/practicum/moviehub/http/MoviesHandler.java
+++ src/main/ru/practicum/moviehub/http/MoviesHandler.java
@@ -1,31 +1,17 @@
 package ru.practicum.moviehub.http;
 
-
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 
-public class MoviesHandler implements HttpHandler {
+public class MoviesHandler extends BaseHttpHandler {
 
     @Override
     public void handle(HttpExchange ex) throws IOException {
         // Напишите реализацию, удовлетворяющую тест
         String method = ex.getRequestMethod();
         if (method.equalsIgnoreCase("GET")) {
-            byte[] bytes =
-                    "[]".getBytes(StandardCharsets.UTF_8);
-            Headers responseHeaders = ex.getResponseHeaders();
-            responseHeaders.set("Content-Type", "application/json; charset=UTF-8");
-            ex.sendResponseHeaders(200, 0);
-
-            try (OutputStream os = ex.getResponseBody()) {
-                os.write(bytes);
-            }
+            sendJson(ex, 200, "[]");
         } else ex.sendResponseHeaders(405, -1);
     }
 }
