97ac17d3c09b3651b15dfaf9afbf10b6cbe3b43d
egor <egorr12122006@gmail.com>
2026-08-14 17:05:48 +0300

    добавить класс ErrorResponse

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -1,5 +1,19 @@
 package ru.practicum.moviehub.api;
 
-public class ErrorResponse {
+import java.util.Arrays;
 
+public class ErrorResponse {
+    private final String error;
+    private final String[] details;
+
+    public ErrorResponse(String error, String[] details) {
+        this.error = error;
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Ошибка " + error +
+                "\nДетали: " + Arrays.toString(details);
+    }
 }
